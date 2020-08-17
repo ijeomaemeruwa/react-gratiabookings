24b6692d2960d241b6a3324981696f021e92bec6
ijeomaemeruwa <helloijeoma@gmail.com>
2020-08-17 17:48:34 +0100

    set-up router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import './App.css';
+import Home from './pages/Home';
+import Deals from './pages/Deals';
+
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      
+    <Router>
+    <div>
+    <nav>
+    <ul>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/deals">Deals</Link></li>
+    </ul>
+    </nav>
+
+  <Switch>
+    <Route path="/deals"><Deals /></Route>
+    <Route path="/"><Home /></Route>
+  </Switch>
     </div>
-  );
+  </Router>
+);
 }
+
 
 export default App;
---
+++ src/pages/Deals.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Deals = () => {
+    return (
+        <div>
+           <h2>Deals</h2> 
+        </div>
+    )
+}
+
+export default Deals;
