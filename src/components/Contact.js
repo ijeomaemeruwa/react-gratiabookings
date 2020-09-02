ddf6c404dc2fb9998261f5df49a630781160b8aa
ijeomaemeruwa <helloijeoma@gmail.com>
2020-09-02 22:36:25 +0100

    added spam filter and name attributes

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -20,27 +20,33 @@ return (
 
 <Form 
   className="padding container col-md-5 col-sm-10 text-center"
-  name="gb-contact"
+  name="contact"
   method="POST"
   data-netlify="true"
+  data-netlify-recaptcha="true"
   onSubmit="submit"
 >
-<input type="hidden" name="form" value="gb-contact" />
+<input type="hidden" name="form" value="contact" />
     <h3>Get In Touch</h3>
     <p>Contact us for your travel enquiries, bookings and cargo services.</p>
   <Form.Group controlId="exampleForm.ControlInput1">
-    <Form.Control type="text" placeholder="FullName" />
+    <Form.Control type="text" placeholder="FullName" name="full-name" />
   </Form.Group><br />
   <Form.Group controlId="exampleForm.ControlInput1">
-    <Form.Control type="text" placeholder="Phone No" />
+    <Form.Control type="text" placeholder="Phone No" name="phone" />
   </Form.Group><br />
   <Form.Group controlId="exampleForm.ControlInput1">
-    <Form.Control type="email" placeholder="Enter valid email" />
+    <Form.Control type="email" placeholder="Enter valid email" name="email" />
   </Form.Group><br />
   <Form.Group controlId="exampleForm.ControlTextarea1">
-    <Form.Control as="textarea" rows="3" placeholder="Enter message or enquiry" />
+    <Form.Control 
+    as="textarea" 
+    rows="3" 
+    placeholder="Enter message or enquiry"
+    name="message"
+    />
   </Form.Group><br />
-
+  <div data-netlify-recaptcha="true"></div>
   <Button variant="primary" type="submit" className="mb-3">Send Message</Button>
 </Form>
 </div>
