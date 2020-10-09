fb0c357b0a2dd4874a15ed19e2c513d2a0757eca
ijeomaemeruwa <helloijeoma@gmail.com>
2020-10-09 13:39:55 +0100

    updates

---
+++ build/precache-manifest.44bd6ed20db3ad25acf6ec7ca0a72592.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbdfdbc32c586d526cb797b1c41daf0a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "819d9ca5fa48d7ce276c",
+    "url": "/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "8dc09004f55bf6afbe95",
+    "url": "/static/css/main.332fb321.chunk.css"
+  },
+  {
+    "revision": "819d9ca5fa48d7ce276c",
+    "url": "/static/js/2.11fd006f.chunk.js"
+  },
+  {
+    "revision": "da1ddcda7183b6313ab6bca1da290ddd",
+    "url": "/static/js/2.11fd006f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8dc09004f55bf6afbe95",
+    "url": "/static/js/main.d501ec54.chunk.js"
+  },
+  {
+    "revision": "36d848941a859b73414e",
+    "url": "/static/js/runtime-main.97140fd0.js"
+  },
+  {
+    "revision": "75e1af96dc0c01929f54ee569c1fa36f",
+    "url": "/static/media/cargo-hero.75e1af96.jpg"
+  },
+  {
+    "revision": "0b003cde93cb0e5558a923be94f9172a",
+    "url": "/static/media/delivery.0b003cde.jpg"
+  },
+  {
+    "revision": "f681f1e96275675016fc6b31dc68fc97",
+    "url": "/static/media/dubai-city.f681f1e9.jpg"
+  },
+  {
+    "revision": "b83eb63f30538beda390d7080973a25c",
+    "url": "/static/media/dubai2.b83eb63f.jpg"
+  },
+  {
+    "revision": "0197f761f6e301f4d02d9117a77029ff",
+    "url": "/static/media/fiji.0197f761.jpg"
+  },
+  {
+    "revision": "87887195bfd142f1ae02535f4914ef51",
+    "url": "/static/media/greece.87887195.jpg"
+  },
+  {
+    "revision": "d259b5f779f9543a0007b698ec2669b2",
+    "url": "/static/media/hero-greece-img.d259b5f7.jpg"
+  },
+  {
+    "revision": "d2778147fa14faac547f7952a6bd86f4",
+    "url": "/static/media/logo.d2778147.svg"
+  },
+  {
+    "revision": "600d89a1031f83fb0e6787405b939c9b",
+    "url": "/static/media/logo2.600d89a1.svg"
+  },
+  {
+    "revision": "f2449c0f0c7397d469ee3496a54202e2",
+    "url": "/static/media/morocco.f2449c0f.jpg"
+  },
+  {
+    "revision": "b423534072a31c5bf5bfc0bc6ae14ede",
+    "url": "/static/media/singapore2.b4235340.jpg"
+  }
+]);
