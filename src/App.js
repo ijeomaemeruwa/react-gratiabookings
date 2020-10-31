b419c5f0595a3ea7fe5066550b128c82cdd82071
ijeomaemeruwa <helloijeoma@gmail.com>
2020-10-31 20:26:13 +0000

    optimized images and added react scroll to nav

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
-import {  Navigation } from './components/Navigation/Navigation';
+import { Navigation } from './components/Navigation/Navigation';
+import ScrollToTop from './components/ScrollToTop/ScrollToTop';
 
 
 const App = () => {
   return (
  <div>
    <Navigation />
+   <ScrollToTop />
  </div>
 );
 }
